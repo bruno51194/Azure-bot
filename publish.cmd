36b5950113f23bcd632818a89578f9173e4ac0ee
Bruno villanueva casero <bruno51194@gmail.com>
2019-02-11 22:59:54 +0100

    Commit inicial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot-bruno51194-Web-Deploy.pubxml -p:Password=eYH4w4fKMT9qJd4rZYsEvHcmeg6X8u648p7DmNKuyHNvncbjlWprjJk9p4iz
+
